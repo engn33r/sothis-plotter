333f5778e3b3302533659216e601b39cb749ac05
engn33r <engn33r@users.noreply.github.com>
2023-07-15 00:00:00 +0000

    .

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # sothis-plotter
+
 Makes pretty plots from sothis JSON output
+
+## Installation
+
+This tool is designed for Python 3.8+. Install the matplotlib dependency with:
+
+```bash
+pip3 install matplotlib
+```
+
+## Example
+
+You can test this tool on an example dataset provided in this repository:
+```bash
+python3 plotter.py -i json_examples/univ2_eth_usdc.json -d
+```
+
+To generate a dataset similar to the example provided in this repository, run sothis to pull storage slot 0 ([`totalSupply`](https://evm.storage/eth/17715162/0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc)) for the Uniswap v2 ETH/USDC pool:
+```bash
+sothis --mode fast_track --source_rpc https://eth.llamarpc.com --contract_address 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc --storage_slot 0 --origin_block 14343000 --terminal_block 14345000 --filename univ2_eth_usdc.json
+```
---
+++ plotter.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import json
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="sothis plotting tool: visualize blockchain data the easy way")
+    parser.add_argument("-i", "--input_file",
+                        help="JSON input file", required=True)
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="Print parsed x and y values")
+    parser.add_argument("-r", "--bytes_right", default=0, type=int,
+                        help="Number of bytes to ignore on the right of the storage slot")
+    parser.add_argument("-l", "--bytes_left", default=0, type=int,
+                        help="Number of bytes to ignore on the left of the storage slot")
+    return parser.parse_args()
+
+def debug(debug_mode: bool, print_msg: str):
+    if(debug_mode):
+        print(print_msg)
+
+def plot_data(json_file: str, debug_mode: bool, bytes_right: int, bytes_left: int):
+    with open(json_file) as json_data:
+        raw_data = json.load(json_data)
+
+    blocknumbers = []
+    y_values = []
+
+    # Extract the data from the JSON file
+    for datapoint in raw_data["state_changes"]:
+        # x-axis shows block number
+        x = int(datapoint["block_number"], 16)
+        debug(debug_mode, "x: " + str(x))
+        blocknumbers.append(x)
+
+
+        # first extract the proper number of bytes from the storage slot value
+        hex_value_without_0x = datapoint["value"][2:] # remove "0x" preceding the value
+        left_cutoff = bytes_left*2
+        right_cutoff = bytes_right*-2
+        if(right_cutoff):
+            hex_value_cut_to_size = hex_value_without_0x[left_cutoff:right_cutoff] # multiply by 2 to convert bytes value to hex, 1 byte = 2 hex chars
+        else: # if right is zero, ignore right splice cut-off and get value until the end
+            hex_value_cut_to_size = hex_value_without_0x[left_cutoff:] # multiply by 2 to convert bytes value to hex, 1 byte = 2 hex chars
+        # y-axis shows storage slot value
+        y = int(hex_value_cut_to_size, 16)
+        debug(debug_mode, "y: " + str(y))
+        y_values.append(y)
+
+    # Plot the data
+    lines = plt.plot(blocknumbers, y_values)
+    plt.xlabel('Block Number')
+    plt.ylabel('Storage Slot Value')
+    plt.setp(lines, color='r')
+    plt.show()
+
+if __name__ == "__main__":
+
+    # Parse input arguments
+    args = parse_args()
+    plot_data(args.input_file, args.debug, args.bytes_right, args.bytes_left)
